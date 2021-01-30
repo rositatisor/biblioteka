668c4360af190bd99dfff7ce2805dd31657cc8f1
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-01-30 12:03:07 +0200

    book entity methods update

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -16,8 +16,12 @@ class BookController extends AbstractController
      */
     public function index(): Response
     {
+        $books = $this->getDoctrine()
+            ->getRepository(Book::class)
+            ->findAll();
+
         return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController'
+            'books' => $books
         ]);
     }
 
@@ -27,8 +31,8 @@ class BookController extends AbstractController
     public function create(): Response
     {
         $authors = $this->getDoctrine()
-                    ->getRepository(Author::class)
-                    ->findAll();
+            ->getRepository(Author::class)
+            ->findAll();
 
         return $this->render('book/create.html.twig', [
             'authors' => $authors
@@ -40,7 +44,11 @@ class BookController extends AbstractController
      */
     public function store(request $r): Response
     {
-        $book = new book;
+        $author = $this->getDoctrine()
+            ->getRepository(Author::class)
+            ->find($r->request->get('book_author_id'));
+
+        $book = new Book;
         $book
             ->setTitle($r->request->get('book_title'))
             ->setIsbn($r->request->get('book_isbn'))
@@ -50,6 +58,68 @@ class BookController extends AbstractController
 
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('book_index');
+    }
+
+    /**
+     * @Route("/book/edit/{id}", name="book_edit", methods={"GET"})
+     */
+    public function edit(int $id): Response
+    {
+        $book = $this->getDoctrine()
+            ->getRepository(Book::class)
+            ->find($id);
+
+        $authors = $this->getDoctrine()
+            ->getRepository(Author::class)
+            ->findAll();
+
+        return $this->render('book/edit.html.twig', [
+            'book' => $book,
+            'authors' => $authors
+        ]);
+    }
+
+    /**
+     * @Route("/book/update/{id}", name="book_update", methods={"POST"})
+     */
+    public function update(request $r, $id): Response
+    {
+        $book = $this->getDoctrine()
+            ->getRepository(Book::class)
+            ->find($id);
+
+        $author = $this->getDoctrine()
+            ->getRepository(Author::class)
+            ->find($r->request->get('books_author'));
+        
+        $book
+            ->setTitle($r->request->get('book_title'))
+            ->setIsbn($r->request->get('book_isbn'))
+            ->setPages($r->request->get('book_pages'))
+            ->setAbout($r->request->get('book_about'))
+            ->setAuthorId($r->request->get('books_author'));
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('book_index');
+    }
+
+    /**
+     * @Route("/book/delete/{id}", name="book_delete", methods={"POST"})
+     */
+    public function delete($id): Response
+    {
+        $book = $this->getDoctrine()
+            ->getRepository(Book::class)
+            ->find($id);
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($book);
         $entityManager->flush();
 
         return $this->redirectToRoute('book_index');
