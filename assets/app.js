68110a1ea19521040ec6638d2ffc97ba00343d86
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-02-04 21:48:28 +0200

    summernote update

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,3 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import 'bootstrap';
@@ -13,10 +6,18 @@ import 'bootstrap';
 // import './bootstrap';
 
 const $ = require('jquery');
-require('summernote');
+const summernote = require('summernote');
 
-$('#summernote').summernote();
+$('#summernote').summernote({
+    toolbar: [
+        ['style', ['bold', 'italic', 'underline', 'clear']],
+        ['font', ['strikethrough', 'superscript', 'subscript']],
+        ['fontsize', ['fontsize']],
+        ['color', ['color']],
+        ['height', ['height']]
+    ]
+});
 
-console.log('summernote');
+console.log(summernote);
 
 console.log('labas');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const Encore = require('@symfony/webpack-encore');
+var Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -17,13 +17,18 @@ Encore
     /*
      * ENTRY CONFIG
      *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    //.addEntry('page1', './assets/page1.js')
+    //.addEntry('page2', './assets/page2.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    // .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     // .splitEntryChunks()
@@ -49,7 +54,6 @@ Encore
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
-
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
@@ -62,15 +66,16 @@ Encore
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
-    // uncomment if you use React
-    //.enableReactPreset()
-
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    // uncomment if you use API Platform Admin (composer require api-admin)
+    //.enableReactPreset()
+    //.addEntry('admin', './assets/admin.js')
     ;
 
 module.exports = Encore.getWebpackConfig();
